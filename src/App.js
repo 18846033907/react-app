8a073dc0485bf41c91c8630fb178d069b75c685b
wenkang <WENKANG.YANG@DELTAWW.COM>
2020-10-20 17:54:06 +0800

    feat: add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Example from "containers/exampleContainer";
+import Home from "containers/homeContainer";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Example />
-    </div>
+    <Router>
+      <Switch>
+      <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/example">
+          <Example />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
