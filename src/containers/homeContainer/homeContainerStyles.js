5a9d369b9ad757df07b13509d5ecc5cbd8fb2369
wenkang <WENKANG.YANG@DELTAWW.COM>
2020-11-04 09:46:06 +0800

    feat: add styled-components

---
+++ src/containers/homeContainer/homeContainerStyles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledDiv = styled.div`
+  color: blue;
+  font-size: 1.2rem;
+`;
--- src/containers/homeContainer/index.js
+++ src/containers/homeContainer/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button, Checkbox } from 'antd';
+import { StyledDiv } from './homeContainerStyles';
 
 const Home = () => {
   return (
     <>
+      <StyledDiv>哈哈哈</StyledDiv>
       <Button type="primary" disabled>
         sdsadasd
       </Button>
-      <Button type="primary">MATERIAL</Button>
+      <Button type="primary">ANTD</Button>
       <Checkbox />
     </>
   );
